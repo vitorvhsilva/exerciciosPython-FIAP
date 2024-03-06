433b91ed571b438466c5cb3097d3c44c6e4c282a
vitorvhsilva <88109337+vitorvhsilva@users.noreply.github.com>
2024-03-05 22:54:57 -0300

    Add files via upload

---
+++ Aula01/aula01.py
@@ -0,0 +1,22 @@
+nome = 'Vagner'
+idade = 38
+altura = 1.70
+usuarioPython = True
+texto = 'Texto Exemplo String.'
+
+print(f'{altura:.2f}')
+
+print(altura.is_integer())
+
+print(len(nome))
+
+print(nome[2:4])
+
+print(texto)
+
+print(texto[0:18])
+
+print(texto[:6])
+
+print(texto[2:10:2]) # sss - start stop step
+
