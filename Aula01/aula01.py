4a0c9424ef0ca67099f11e678f1e0fafba874755
vitorvhsilva <88109337+vitorvhsilva@users.noreply.github.com>
2024-03-05 23:09:36 -0300

    Add files via upload

--- Aula01/aula01.py
+++ Aula01/aula01.py
@@ -3,6 +3,9 @@ idade = 38
 altura = 1.70
 usuarioPython = True
 texto = 'Texto Exemplo String.'
+binario = '010101010101010101'
+ternario = '123123123123123123'
+
 
 print(f'{altura:.2f}')
 
@@ -19,4 +22,8 @@ print(texto[0:18])
 print(texto[:6])
 
 print(texto[2:10:2]) # sss - start stop step
+
+print(binario[1:10:2])
+
+print(ternario[2:10:3])
 
