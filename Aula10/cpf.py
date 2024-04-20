df48988107076ac56d07b16087788061445bed2d
vitorvhsilva <88109337+vitorvhsilva@users.noreply.github.com>
2024-04-20 00:54:39 -0300

    Add files via upload

--- Aula10/cpf.py
+++ Aula10/cpf.py
@@ -1,45 +1,8 @@
-import random
-
-# cpf = '' 
-# calculo = 0
-# j = ''
-# k = ''
-
-# while len(cpf) <= 9:
-#     cpf += str(random.randint(0, 9))
-
-# for i in cpf:
-#     mult = 10
-#     calculo += int(i) * mult
-#     mult -= 1
-
-# if calculo % 11 == 0 or calculo % 11 == 1: 
-#     j = 0
-# elif calculo % 11 >= 2:
-#     j = (11 - (calculo % 11))
-
-# cpf += str(j)
-
-# calculo = 0
-
-# for k in cpf:
-#     mult = 11
-#     calculo += int(k) * mult
-#     mult -= 1
-
-# if calculo % 11 == 0 or calculo % 11 == 1: 
-#     k = 0
-# elif calculo % 11 >= 2:
-#     k = (11 - (calculo % 11))
-
-# cpf += str(k)
-
-# print(cpf)
-
 def validaCPF(cpf):
     calculo = 0
 
     j = 0
+    k = 0
 
     mult = 10
 
@@ -47,16 +10,23 @@ def validaCPF(cpf):
         calculo += int(i) * mult
         mult -= 1
 
-    print(calculo)
+    if calculo % 11 >= 2:
+        j = (11 - (calculo % 11))
+        
+    mult = 11
+    calculo = 0
 
-    # if calculo % 11 == 0 or calculo % 11 == 1: 
-    #     j = 0
-    # elif calculo % 11 >= 2:
-    #     j = (11 - (calculo % 11))
+    for i in cpf[:10]:
+        calculo += int(i) * mult
+        mult -= 1
 
-    # print(j)
+    if calculo % 11 >= 2:
+        k = (11 - (calculo % 11))
 
-    # if cpf[9] == str(j):
-    #     print('boa')
+    if cpf[9] == str(j) and cpf[10] == str(k):
+        print('CPF Válido! ')
+    else: 
+        print('CPF inválido! ')
+        
 
-validaCPF('52439200883')
+validaCPF('52439200882')
